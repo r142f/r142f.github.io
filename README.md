ec390e2fb9c93b43866549742490c9b95f096d88
r142f <h2riaff@gmail.com>
2019-02-04 00:28:10 +0300

    Добавил проект pics, обновил README

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - <https://r142f.github.io/random-profiles>
 - <https://r142f.github.io/stock-trader>
+- <https://r142f.github.io/pics>
---
+++ pics/precache-manifest.3985d74a37e2d5722d5c5e38b36b30c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "268314b963fb3793dbc6",
+    "url": "/pics/static/css/main.c393575b.chunk.css"
+  },
+  {
+    "revision": "268314b963fb3793dbc6",
+    "url": "/pics/static/js/main.268314b9.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/pics/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "9e0d4f6ad25f02f07e2d",
+    "url": "/pics/static/js/runtime~main.9e0d4f6a.js"
+  },
+  {
+    "revision": "8878257cd1e73ad413c947b48343b691",
+    "url": "/pics/index.html"
+  }
+];
