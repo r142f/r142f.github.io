ad54dc2ece72bbbdf403462eb941300cea18f1d2
r142f <h2riaff@gmail.com>
2019-02-04 14:15:37 +0300

    Добавил проект videos, обновил README

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - <https://r142f.github.io/random-profiles>
 - <https://r142f.github.io/stock-trader>
 - <https://r142f.github.io/pics>
+- <https://r142f.github.io/videos>
---
+++ videos/precache-manifest.009c1daddefce07f5a588c7b3819b3cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4352afda10ae14a801e2",
+    "url": "/videos/static/css/main.783fd1d0.chunk.css"
+  },
+  {
+    "revision": "4352afda10ae14a801e2",
+    "url": "/videos/static/js/main.4352afda.chunk.js"
+  },
+  {
+    "revision": "99d9b32e70d8f9dc890d",
+    "url": "/videos/static/js/1.99d9b32e.chunk.js"
+  },
+  {
+    "revision": "fb30758431df4dcb2543",
+    "url": "/videos/static/js/runtime~main.fb307584.js"
+  },
+  {
+    "revision": "225917d559d7e9799918c5e574146a6c",
+    "url": "/videos/index.html"
+  }
+];
